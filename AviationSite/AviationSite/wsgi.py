aef8924cb34bce83e5a6671a41fa8ff87964b48d
Marlster <marley7898@gmail.com>
2019-02-17 18:08:29 +0000

    changed wsgi settings again

--- AviationSite/AviationSite/wsgi.py
+++ AviationSite/AviationSite/wsgi.py
@@ -11,7 +11,7 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/Documents/Website/AviationWebsite'
+path = '/Documents/Website/AviationWebsite/AviationSite'
 if path not in sys.path:
     sys.path.append(path)
 
