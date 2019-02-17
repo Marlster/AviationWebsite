ab626e181295953cb93f90dd3c187d195ce65862
Marlster <marley7898@gmail.com>
2019-02-17 19:01:15 +0000

    changed settings.py

--- AviationSite/AviationSite/settings.py
+++ AviationSite/AviationSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*Bd.(.zoottw${|JB_CY<sH~Za~fF<;=QPS.Us<gAzhP8f_Co(hdR=S7!(O0^n{'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.avsoc.co.uk', '127.0.0.1', u'mmc21.host.cs.st-andrews.ac.uk','av-soc-website.herokuapp.com']
 
